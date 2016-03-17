68fef8982f57b2f5d0ba38b8a66bc16347b474b2
hirama-akihiro <hirama.akihiro.ha@gmail.com>
2016-03-17 16:33:37 +0900

    シングルタッチの回転操作に対応

---
+++ Umol-PDBViewer/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TouchManager : MonoBehaviour {
+
+	public GameObject molecule;
+	private int prevTouchCount;
+	private int nowTouchCount;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nowTouchCount = Input.touchCount;
+
+		if(prevTouchCount != 1 && nowTouchCount == 1) { OneTouchBegin(); }
+		else if(prevTouchCount == 1 && nowTouchCount == 1) { OneTouching(); }
+		else if(prevTouchCount == 1 && nowTouchCount != 1) { OneTouchEnd(); }
+		else if(prevTouchCount != 2 && nowTouchCount == 2) { TwoTouchBegin(); }
+		else if(prevTouchCount == 2 && nowTouchCount == 2) { TwoTouching(); }
+		else if(prevTouchCount == 2 && nowTouchCount != 2) { TwoTouchEnd(); }
+
+		prevTouchCount = nowTouchCount;
+	}
+
+	private void OneTouchBegin()
+	{
+
+	}
+
+	private void OneTouching()
+	{
+		Touch touch = Input.GetTouch(0);
+		molecule.transform.Rotate(new Vector3(touch.deltaPosition.y, touch.deltaPosition.x, 0));
+	}
+
+	private void OneTouchEnd()
+	{
+
+	}
+
+	private void TwoTouchBegin()
+	{
+
+	}
+
+	private void TwoTouching()
+	{
+
+	}
+
+	private void TwoTouchEnd()
+	{
+
+	}
+}
--- Umol-PDBViewer/Assets/Scripts/View3DManager.cs
+++ Umol-PDBViewer/Assets/Scripts/View3DManager.cs
@@ -6,6 +6,7 @@ using System.IO;
 public class View3DManager : MonoBehaviour {
 
 	public Text mTextPDBID;
+	public GameObject molecule;
 
 	// Use this for initialization
 	void Start () {
@@ -46,11 +47,43 @@ public class View3DManager : MonoBehaviour {
 			PdbFile pdbfile = PDBParser.ParsePdbFile(path);
 
 			// 原子球を読み込み座標に表示
-			foreach(PdbRecord record in pdbfile.PdbATOMRecord)
+			for (int i = 0, c = pdbfile.PdbATOMRecord.Count - 1; i < c; i++)
 			{
+				PdbRecord record1 = pdbfile.PdbATOMRecord[i];
+				PdbRecord record2 = pdbfile.PdbATOMRecord[i + 1];
+
+				if(record1.AminoNo + 1 != record2.AminoNo) { continue; }
+
+				// 原子
 				GameObject prefab = Resources.Load("Prefabs/AtomSphere") as GameObject;
-				GameObject atomSphere = Instantiate(prefab, new Vector3((float)record.Point.PtX, (float)record.Point.PtY, (float)record.Point.PtZ), Quaternion.identity) as GameObject;
+				Vector3 atomPos = new Vector3((float)record1.Point.PtX, (float)record1.Point.PtY, (float)record1.Point.PtZ);
+				GameObject atom = Instantiate(prefab, atomPos, Quaternion.identity) as GameObject;
+
+				// 辺
+				prefab = Resources.Load("Prefabs/AtomCylinder") as GameObject;
+				GameObject edde = Instantiate(prefab, new Vector3((float)((record1.Point.PtX + record2.Point.PtX) / 2), (float)((record1.Point.PtY + record2.Point.PtY) / 2), (float)((record1.Point.PtZ + record2.Point.PtZ) / 2)), Quaternion.identity) as GameObject;
+				edde.transform.localScale = new Vector3(0.4f, (float)DPoint.Distance(record1.Point, record2.Point) / 2, 0.4f);
+
+				atom.transform.parent = molecule.transform;
+				edde.transform.parent = molecule.transform;
 			}
 		}
+	}
+
+	public double CalcDotProduct(Vector2 left, Vector2 right)
+	{
+		return left.x * right.x + left.y * right.y;
+	}
+
+	public double CalcAngleOfVector2(Vector2 left, Vector2 right)
+	{
+		double lenA = left.magnitude;
+		double lenB = right.magnitude;
+
+		double cosSeta = CalcDotProduct(left, right) / (lenA * lenB);
+
+		double radian = Mathf.Acos((float)cosSeta);
+
+		return radian * 180.0 / Mathf.PI;
 	}
 }
